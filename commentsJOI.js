99ec15ba5c84fa6ded1e8e0be6fdcf1dd080448c
Samaresh Das <samareshmail679@gmail.com>
2022-08-09 13:01:47 +0530

    added post validations

--- commentsJOI.js
+++ commentsJOI.js
@@ -1,9 +1,9 @@
 const Joi = require("joi");
 
 module.exports.commentsJoiSchema = Joi.object({
-  comments: Joi.object({
+  comment: Joi.object({
     username: Joi.string().required(),
     email: Joi.string().required(),
-    comment: Joi.string().required(),
+    comments: Joi.string().required(),
   }).required(),
 });
--- index.js
+++ index.js
@@ -9,13 +9,25 @@ const flash = require("connect-flash");
 const Comments = require("./models/Comments");
 const { commentsJoiSchema } = require("./commentsJOI");
 const ExpressError = require("./ExpressError");
+const session = require("express-session");
 
 app.engine("ejs", ejsmate);
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.use(methodOverride("_method"));
-app.use(flash());
+
+const sessionConfig = {
+  secret: "rjhf3ruihf",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7, //mili-seconds in a week XD
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
 
 mongoose.connect("mongodb://localhost:27017/practiceCrudForm");
 const db = mongoose.connection;
@@ -33,6 +45,13 @@ const validateComment = (req, res, next) => {
     next();
   }
 };
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -43,9 +62,10 @@ app.get("/comments", async (req, res) => {
   res.render("comments", { comment });
 });
 
-app.post("/comments", async (req, res) => {
+app.post("/comments", validateComment, async (req, res) => {
   const comment = new Comments(req.body.comment);
   await comment.save();
+  req.flash("success", "Comment added");
   res.redirect("/comments");
 });
 
@@ -56,6 +76,7 @@ app.get("/comments/:id/edit", async (req, res) => {
   if (!foundComment) {
     return res.redirect("/comments");
   }
+
   res.render("edit", { foundComment });
 });
 
@@ -76,6 +97,17 @@ app.delete("/comments/:id", async (req, res) => {
 
 app.get("/form", (req, res) => {
   res.render("form");
+});
+
+app.all("*", (req, res, next) => {
+  //all indicates do this for every single requests and * indicates all paths and also where you declare this matters. I recommend declaring it at last before err func
+  next(new ExpressError("Page Not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err; //giving them a default using equals sign
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(port, () => {
